e151cef33b577c0d9da88168f70084644b645ae2
congyuwang <leonwang998@gmail.com>
2023-06-26 11:59:14 +0800

    add docstring to public api.

--- include/socket_manager/msg_sender.h
+++ include/socket_manager/msg_sender.h
@@ -6,10 +6,22 @@
 
 namespace socket_manager {
 
+  /**
+   * Use MsgSender to send messages to the peer.
+   */
   class MsgSender {
 
   public:
 
+    /**
+     * Send a message to the peer.
+     *
+     * This method is thread safe.
+     * This method does not implement backpressure
+     * (i.e., it caches all the messages in memory).
+     *
+     * @param data the message to send
+     */
     void send(const std::string &data);
 
     ~MsgSender();
--- include/socket_manager/socket_manager.h
+++ include/socket_manager/socket_manager.h
@@ -9,39 +9,125 @@
 
 namespace socket_manager {
 
+  /**
+   * Manages a set of sockets.
+   *
+   * Inherit from SocketManager and implement the virtual methods:
+   * `on_connect`, `on_connection_close`, `on_listen_error`, `on_connect_error`,
+   * `on_message` as callbacks. Note that these callbacks shouldn't block.
+   */
   class SocketManager {
 
   public:
 
     SocketManager();
 
+    /**
+     * Listen on the given address.
+     *
+     * # Errors
+     * Throws `std::runtime_error` if the address is invalid.
+     *
+     * @param addr: the ip address to listen to (support both ipv4 and ipv6).
+     */
     void listen_on_addr(const std::string &addr);
 
+    /**
+     * Connect to the given address.
+     *
+     * # Errors
+     * Throws `std::runtime_error` if the address is invalid.
+     *
+     * @param addr: the ip address to listen to (support both ipv4 and ipv6).
+     */
     void connect_to_addr(const std::string &addr);
 
+    /**
+     * Abort a connection.
+     *
+     * Does nothing if the connection is already closed or the id is invalid.
+     *
+     * @param id the id of the connection to abort.
+     */
     void cancel_connection(unsigned long long id);
 
+    /**
+     * Cancel listening on the given address.
+     *
+     * # Errors
+     * Throw `std::runtime_error` if the address is invalid.
+     *
+     * @param addr cancel listening on this address.
+     */
     void cancel_listen_on_addr(const std::string &addr);
 
+    /**
+     * Join and wait on the `SocketManager` background runtime.
+     */
     void join();
 
+    /**
+     * Detach the `SocketManager`'s background runtime.
+     */
     void detach();
 
     /// virtual methods: must be thread safe
 
+    /**
+     * Called when a new connection is established.
+     *
+     * It should be non-blocking.
+     *
+     * @param id the id of the connection.
+     * @param local_addr the local address of the connection.
+     * @param peer_addr the peer address of the connection.
+     * @param sender a `MsgSender` object that can be used to send messages to
+     *              the peer.
+     */
     virtual void on_connect(unsigned long long id,
                             const std::string &local_addr,
                             const std::string &peer_addr,
                             std::shared_ptr<MsgSender> sender) = 0;
 
+    /**
+     * Called when a connection is closed.
+     *
+     * It should be non-blocking.
+     *
+     * @param id the id of the connection.
+     */
     virtual void on_connection_close(unsigned long long id) = 0;
 
+    /**
+     * Called when an error occurs when listening on the given address.
+     *
+     * Should be non-blocking.
+     *
+     * @param addr the address that failed to listen on.
+     * @param err the error message.
+     */
     virtual void on_listen_error(const std::string &addr,
                                  const std::string &err) = 0;
 
+    /**
+     * Called when an error occurs when connecting to the given address.
+     *
+     * Should be non-blocking.
+     *
+     * @param addr the address that failed to connect to.
+     * @param err the error message.
+     */
     virtual void on_connect_error(const std::string &addr,
                                   const std::string &err) = 0;
 
+    /**
+     * Called when a message is received.
+     *
+     * Should be non-blocking.
+     *
+     * @param id the id of the connection.
+     * @param data the message received.
+     */
     virtual void on_message(unsigned long long id,
                             const std::string &data) = 0;
 
