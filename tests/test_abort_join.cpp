d030c6d1c166cef5f8c4cf1b6b3e2131bf67cb07
congyuwang <leonwang998@gmail.com>
2023-06-28 13:16:04 +0800

    add abort test

---
+++ tests/test_abort_join.cpp
@@ -0,0 +1,22 @@
+#include <thread>
+#include "test_utils.h"
+
+using namespace socket_manager;
+
+void abort_manager(SocketManager &manager) {
+  manager.abort();
+}
+
+int test_abort_join(int argc, char **argv) {
+  auto nothing_cb = std::make_shared<DoNothingConnCallback>();
+  SocketManager nothing(nothing_cb);
+
+  std::thread abort_t(abort_manager, std::ref(nothing));
+
+  // shouldn't time out
+  nothing.join();
+
+  abort_t.join();
+
+  return 0;
+}
