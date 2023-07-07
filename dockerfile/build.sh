9d9935c643695bf60695b15afea275c305166ea6
congyuwang <leonwang998@gmail.com>
2023-07-07 17:51:04 +0800

    add dockerfile for dev environment

---
+++ dockerfile/build.sh
@@ -0,0 +1,2 @@
+docker build -f ./dockerfile/dev-containers/focal/Dockerfile -t socket-manager-dev:focal .
+docker build -f ./dockerfile/dev-containers/jammy/Dockerfile -t socket-manager-dev:jammy .
