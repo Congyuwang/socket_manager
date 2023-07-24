adc4cc11faba85d6a292ce567d2d04eba98d4763
Congyuwang <leonwang998@gmail.com>
2023-07-24 23:23:12 +0800

    simplify the async send api algorithm

--- src/msg_sender.rs
+++ src/msg_sender.rs
@@ -1,13 +1,10 @@
-use crate::c_api::callbacks::WakerObj;
 use async_ringbuf::AsyncHeapProducer;
 use std::future::Future;
 use std::pin::pin;
 use std::task::Poll::Ready;
-use std::task::{Context, Poll};
+use std::task::{Context, Poll, Waker};
 use tokio::runtime::Handle;
 use tokio::sync::mpsc::UnboundedSender;
-
-pub const PENDING_CODE: i64 = -1;
 
 /// Sender Commands other than bytes.
 pub(crate) enum SendCommand {
@@ -80,45 +77,29 @@ impl CMsgSender {
         Ok(())
     }
 
-    /// Try sending bytes.
+    /// Try sending bytes (the async api).
     ///
-    /// Returning -1 to indicate pending.
-    pub fn try_send(&mut self, bytes: &[u8], waker_obj: Option<WakerObj>) -> std::io::Result<i64> {
+    /// Unless the buffer is empty, it shouldn't return 0.
+    pub fn send_async(&mut self, bytes: &[u8], waker: Waker) -> Poll<std::io::Result<usize>> {
         if bytes.is_empty() {
-            return Ok(0);
+            return Ready(Ok(0));
         }
         if self.buf_prd.is_closed() {
-            return Err(std::io::Error::new(
+            return Ready(Err(std::io::Error::new(
                 std::io::ErrorKind::WriteZero,
                 "connection closed",
-            ));
+            )));
         }
         let n = self.buf_prd.as_mut_base().push_slice(bytes);
+        // Immediately ready if the buf is not empty.
+        // This might prevent registering the waker
+        // when the buffer is not empty.
         if n > 0 {
-            return Ok(n as i64);
+            return Ready(Ok(n));
         }
-        // n = 0, not closed
-        if let Some(waker_obj) = waker_obj {
-            // some waker, wait on the waker
-            let waker = unsafe { waker_obj.make_waker() };
-            match pin!(self.buf_prd.wait_free(1)).poll(&mut Context::from_waker(&waker)) {
-                Ready(_) => {
-                    // might be ready on closed
-                    if self.buf_prd.is_closed() {
-                        return Err(std::io::Error::new(
-                            std::io::ErrorKind::WriteZero,
-                            "connection closed",
-                        ));
-                    }
-                    let n = self.buf_prd.as_mut_base().push_slice(bytes);
-                    Ok(n as i64)
-                }
-                Poll::Pending => Ok(PENDING_CODE),
-            }
-        } else {
-            // no waker, return 0
-            Ok(0)
-        }
+        // basically just register the waker, and return pending.
+        let _ = pin!(self.buf_prd.wait_free(1)).poll(&mut Context::from_waker(&waker));
+        Poll::Pending
     }
 
     pub fn flush(&mut self) -> std::io::Result<()> {
