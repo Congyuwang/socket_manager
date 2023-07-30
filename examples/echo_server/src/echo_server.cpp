c6bf94dc8b9380e40afad57627782bc5fb0124c2
Congyuwang <leonwang998@gmail.com>
2023-07-30 19:57:04 +0800

    clangd check for echo server

--- examples/echo_server/src/echo_server.cpp
+++ examples/echo_server/src/echo_server.cpp
@@ -1,9 +1,9 @@
-#include <thread>
 #include <iostream>
-#include <socket_manager.h>
-#include <variant>
-#include <unordered_map>
 #include <memory>
+#include <socket_manager.h>
+#include <thread>
+#include <unordered_map>
+#include <variant>
 
 /**
  * UniqueWaker is a wrapper of `socket_manager::Waker`
@@ -13,14 +13,10 @@ class WrapWaker : public socket_manager::Notifier {
 public:
   explicit WrapWaker(socket_manager::Waker &&wake) : waker(std::move(wake)) {}
 
-  void set_waker(socket_manager::Waker &&wake) {
-    waker = std::move(wake);
-  }
+  void set_waker(socket_manager::Waker &&wake) { waker = std::move(wake); }
 
 private:
-  void wake() override {
-    waker.wake();
-  }
+  void wake() override { waker.wake(); }
 
   socket_manager::Waker waker;
 };
@@ -33,10 +29,9 @@ private:
  */
 class EchoReceiver : public socket_manager::MsgReceiverAsync {
 public:
-  explicit EchoReceiver(
-          std::shared_ptr<socket_manager::MsgSender> &&sender,
-          const std::shared_ptr<WrapWaker> &waker
-  ) : waker(waker), sender(std::move(sender)) {};
+  explicit EchoReceiver(std::shared_ptr<socket_manager::MsgSender> &&sender,
+                        const std::shared_ptr<WrapWaker> &waker)
+      : waker(waker), sender(std::move(sender)){};
 
   /**
    * Release resources to break potential ref cycles.
@@ -47,7 +42,8 @@ public:
   }
 
 private:
-  long on_message_async(std::string_view data, socket_manager::Waker &&wake) override {
+  auto on_message_async(std::string_view data, socket_manager::Waker &&wake)
+      -> long override {
     waker->set_waker(std::move(wake));
     return sender->send_async(data);
   };
@@ -73,7 +69,8 @@ private:
     conn->start(std::move(recv), std::move(waker));
   }
 
-  void on_connection_close(const std::string &local_addr, const std::string &peer_addr) override {
+  void on_connection_close(const std::string &local_addr,
+                           const std::string &peer_addr) override {
     {
       std::lock_guard<std::mutex> lock(mutex);
       auto find = receivers.find(local_addr + peer_addr);
@@ -82,17 +79,21 @@ private:
         find->second->close();
         receivers.erase(find);
       } else {
-        throw std::runtime_error("connection not found: " + local_addr + " -> " + peer_addr);
+        throw std::runtime_error("connection not found: " + local_addr +
+                                 " -> " + peer_addr);
       }
     }
-    std::cout << "connection closed: " << local_addr << " -> " << peer_addr << std::endl;
+    std::cout << "connection closed: " << local_addr << " -> " << peer_addr
+              << std::endl;
   }
 
-  void on_listen_error(const std::string &addr, const std::string &err) override {
+  void on_listen_error(const std::string &addr,
+                       const std::string &err) override {
     throw std::runtime_error("listen error: addr=" + addr + ", " + err);
   }
 
-  void on_connect_error(const std::string &addr, const std::string &err) override {
+  void on_connect_error(const std::string &addr,
+                        const std::string &err) override {
     throw std::runtime_error("connect error: addr=" + addr + ", " + err);
   }
 
@@ -100,7 +101,7 @@ private:
   std::unordered_map<std::string, std::shared_ptr<EchoReceiver>> receivers;
 };
 
-int main() {
+auto main() -> int {
   // start the server
   auto callback = std::make_shared<EchoCallback>();
   auto manager = socket_manager::SocketManager(callback);
